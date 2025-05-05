c61867e70bd8ae4c956131fb8f588eb6bf46a6ff
Sofia Vognild Burkow <s.v.burkow@student.tudelft.nl>
2025-05-05 09:59:26 +0200

    Model and lib imported correctly. Still issues with preparing the input data for prediction.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,20 @@
 FROM python:3.12.9-slim
 
 WORKDIR /root/
+
 COPY requirements.txt .
 
-RUN python -m pip install --upgrade pip &&\
+RUN apt-get update && \
+	apt-get install -y git && \
+	apt-get install -y curl 
+
+RUN python -m pip install --upgrade pip && \
 	pip install -r requirements.txt
+
+# Download the pre-trained models
+RUN mkdir -p models && \
+    curl -L https://github.com/remla25-team1/model-training/releases/download/v0.0.2/c1_BoW_Sentiment_Model.pkl -o models/c1_BoW_Sentiment_Model.joblib && \
+	curl -L https://github.com/remla25-team1/model-training/releases/download/v0.0.2/v0.0.2_Sentiment_Model.pkl -o models/v0.0.2_Sentiment_Model.joblib
 
 COPY src src
 
--- src/serve_model.py
+++ src/serve_model.py
@@ -2,20 +2,13 @@
 Flask API of the tweet sentiment detection model.
 """
 
-import joblib
+import joblib, pickle
 from flask import Flask, jsonify, request
 from flasgger import Swagger
-
-# from lib_ml.preprocessing import prepare  
-# import model from an accessible location with a public link
+from lib_ml.preprocessing import Preprocessor
 
 app = Flask(__name__)
 swagger = Swagger(app)
-
-@app.route("/", methods=["GET"])
-def index():
-    return "REST API for model-service"
-
 
 @app.route("/predict", methods=['POST'])
 def predict():
@@ -38,21 +31,26 @@ def predict():
                     example: This is an example of a tweet.
     responses:
         200:
-            description: "The result of the classification: 'positive' or 'negative'."
+            description: "The result of the classification: 'positive' (1) or 'negative' (0)."
     
     """
     input_data = request.get_json()
     tweet = input_data.get('tweet')
-    processed_tweet = prepare(tweet)
-    model = joblib.load('output/model.joblib')
+    # Preprocess the input
+    preprocessor = Preprocessor()
+    processed_tweet = preprocessor.process_item(tweet)
+    # Transform data
+    cv = pickle.load(open('models/c1_BoW_Sentiment_Model.joblib', "rb"))
+    processed_tweet = cv.transform([processed_tweet]).toarray()
+    # Predict
+    model = joblib.load('models/v0.0.2_Sentiment_Model.joblib')
     prediction = model.predict(processed_tweet)[0]
 
     res = {
         "result": prediction,
-        "classifier": "decision tree",
+        "classifier": "Naive Bayes classifier",
         "tweet": tweet
     }
-    print(tweet)
 
     return jsonify(res)
 
@@ -60,7 +58,7 @@ def predict():
 @app.route('/dumbpredict', methods=['POST'])
 def dumb_predict():
     """
-    Predict the sentiment of a tweet (dumb model: always predicts 'positive').
+    Predict the sentiment of a tweet (dumb model: always predicts 'positive' (1)).
     ---
     consumes: 
         - application/json
@@ -85,12 +83,11 @@ def dumb_predict():
     tweet = input_data.get('tweet')
 
     return jsonify({
-        "result": "Positive",
-        "classifier": "decision tree",
+        "result": 1,
+        "classifier": "Naive Bayes classifier",
         "tweet": tweet
     })
 
 
 if __name__ == '__main__':
-    # clf = joblib.load('output/model.joblib')
     app.run(host="0.0.0.0", port=8080, debug=True)
